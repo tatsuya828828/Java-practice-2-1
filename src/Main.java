a2f11b85b291eba1c69dc78b511140dcf3beb856
tatsuya828828 <kkjy011894@gmail.com>
2020-07-03 15:32:07 +0900

    [finish]1-3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 
 public class Main {
 	public static void main(String[] args) {
-		String s1 = "スッキリJava";
-		String s2 = "Java";
-		String s3 = "java";
-		String s4 = "";
+		String s1 = "Java and JavaScript";
+		// contains: 一部に引数の文字列が、含まれているかどうか調べる。
+		if(s1.contains("Java")) {
+			System.out.println("文字列s1は、Javaを含んでいます");
+		}
+		// endWith: 引数の文字列で終わっているか確かめる。
+		if(s1.endsWith("Java")) {
+			System.out.println("文字列s1は、Javaが末尾にあります。");
+		}
+		// indexOf: 引数の文字列が最初に登場するのは何文字目か調べる。
+		System.out.println("文字列s1で最初にJavaが登場する位置は"+(s1.indexOf("Java")+1)+"文字目");
+		// lastIndexOf: 引数の文字列が最後に登場する位置が何文字目か調べる。
+		System.out.println("文字列s1で最後にJavaが登場する位置は"+(s1.lastIndexOf("Java")+1)+"文字目");
 
-		if(s3.equals(s2)) {
-			System.out.println("s2とs3は全く同じ文字です。");
+		String s2 = "Java programming";
+		// substring: 指定一から始まる文字列を任意の長さだけ切り出す
+		// 先頭を指定する時は0
+		System.out.println("文字列s2の4文字目以降は"+s2.substring(3));
+		System.out.println("文字列s2の4〜8文字目は"+s2.substring(3, 8));
+		System.out.println("s2の内容は"+s2);
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 100; i++) {
+			sb.append("Java");
 		}
-		if(s3.equalsIgnoreCase(s2)) {
-			System.out.println("s2とs3は、文字の大きさを考えなければ同じ文字です。");
-		}
-		System.out.println("s1の文字の長さは"+s1.length()+"文字です");
-		if(s4.isEmpty()) {
-			System.out.println("s4は、空文字です");
-		}
+		String s = sb.toString();
+		System.out.println(s);
 	}
 }
